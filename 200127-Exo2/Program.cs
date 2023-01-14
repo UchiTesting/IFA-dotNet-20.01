390ba47d667e7792af21c29b21a7213255498841
UchiTesing <uchitesting57@gmail.com>
2023-01-14 21:38:09 +0100

    Update Exo2

--- 200127-Exo2/Program.cs
+++ 200127-Exo2/Program.cs
@@ -6,42 +6,46 @@
  * Quit Q
  * Afficher le contenu du tableau
  * 
+ * Dynamic array program
+ * Ask for integers
+ * Quit if input is q
+ * 
  */
 
-namespace _200127_Exo2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Exo 2: Tableau de n entiers\n========\n");
-            int taille = 0;
-            int[] tab = new int[taille];
-            string valeurSaisie = string.Empty;
-            int n;
+namespace _200127_Exo2;
 
-            do
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exo 2: Tableau de n entiers\n========\n");
+        int taille = 0;
+        int[] tab = new int[taille];
+        string valeurSaisie = string.Empty;
+        int n;
+
+        do
+        {
+            Console.Write("Valeur (q -> quitter): ");
+            valeurSaisie = Console.ReadLine();
+            if (int.TryParse(valeurSaisie, out n))
             {
-                Console.Write("Valeur (q -> quitter): ");
-                valeurSaisie = Console.ReadLine();
-                if (int.TryParse(valeurSaisie, out n))
-                {
                 taille++;
                 Array.Resize(ref tab, taille);
                 tab[taille - 1] = n;
-                } else
-                {
-                    if (valeurSaisie != "q")
-                        Console.WriteLine("Please input an integer.");
-                }
-
-            } while (!valeurSaisie.Equals("q"));
-
-            // Affichage du tableau
-            foreach (var item in tab)
-            {
-                Console.WriteLine(item);
             }
+            else
+            {
+                if (valeurSaisie != "q")
+                    Console.WriteLine("Please input an integer.");
+            }
+
+        } while (!valeurSaisie.Equals("q"));
+
+        // Affichage du tableau
+        foreach (var item in tab)
+        {
+            Console.WriteLine(item);
         }
     }
 }
